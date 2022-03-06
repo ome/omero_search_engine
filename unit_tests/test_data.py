ed91ba12da304abbe8789dec2979579cc8cb6c60
khaledk2 <khaledk2@hotmail.com>
2022-03-06 21:19:31 +0000

    fix test data to work  with multi Or condition

--- unit_tests/test_data.py
+++ unit_tests/test_data.py
@@ -3,8 +3,8 @@ sql="select current_database()"
 valid_and_filters = [{"name": "Organism", "value": "Homo sapiens", "operator": "equals"},
                      {"name": "Antibody Identifier", "value": "CAB034889", "operator": "equals"}]
 
-valid_or_filters = [{"name": "Organism Part", "value": "Prostate", "operator": "equals"},
-                    {"name": "Organism Part Identifier", "value": "T-77100", "operator": "equals"}]
+valid_or_filters = [[{"name": "Organism Part", "value": "Prostate", "operator": "equals"},
+                    {"name": "Organism Part Identifier", "value": "T-77100", "operator": "equals"}]]
 
 not_valid_and_filters = [{"name": "Organism", "value": "Mus musculus"},
                {"name": "Organism Part", "operator": "equals", "value": "Prostate"}]
