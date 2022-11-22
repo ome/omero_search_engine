a47661353040d23954a9909f074266b595fee3c1
khaledk2 <khaledk2@hotmail.com>
2022-11-22 18:20:01 +0000

    fix import issue

--- omero_search_engine/cache_functions/elasticsearch/backup_restores.py
+++ omero_search_engine/cache_functions/elasticsearch/backup_restores.py
@@ -13,7 +13,9 @@ def get_backup_repository(es):
 def backup_indices_data():
     es = search_omero_app.config.get("es_connector")
     get_backup_repository(es)
-    from transform_data import get_all_indexes
+    from omero_search_engine.cache_functions.elasticsearch.transform_data import (
+        get_all_indexes,
+    )
 
     index_body = {"indices": ",".join(get_all_indexes())}
     # tryng to delete the snapshot if it exists.
