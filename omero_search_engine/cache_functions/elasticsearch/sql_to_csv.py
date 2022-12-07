e6337418f8c57706af8e7aa1545d116036bec39b
khaledk2 <khaledk2@hotmail.com>
2022-12-07 10:31:22 +0000

    Add resource to the index even it has not key-value pairs

--- omero_search_engine/cache_functions/elasticsearch/sql_to_csv.py
+++ omero_search_engine/cache_functions/elasticsearch/sql_to_csv.py
@@ -35,8 +35,8 @@ screen.id as screen_id, screen.name as screen_name,
 plate.id as plate_id, plate.name as plate_name,
 well.id as well_id,wellsample.id as wellsample_id
 from image
-inner join imageannotationlink on image.id =imageannotationlink.parent
-inner join annotation_mapvalue on
+left join imageannotationlink on image.id =imageannotationlink.parent
+left join annotation_mapvalue on
 annotation_mapvalue.annotation_id=imageannotationlink.child
 left join datasetimagelink on datasetimagelink.child=image.id
 left join dataset on datasetimagelink.parent=dataset.id
@@ -68,8 +68,8 @@ select plate.id, plate.owner_id, plate.group_id,plate.description,
 plate.name as name, annotation_mapvalue.name as mapvalue_name,
 annotation_mapvalue.value as mapvalue_value,
 annotation_mapvalue.index as mapvalue_index from plate
-inner join plateannotationlink on plate.id=plateannotationlink.parent
-inner join annotation_mapvalue on
+left join plateannotationlink on plate.id=plateannotationlink.parent
+left join annotation_mapvalue on
 annotation_mapvalue.annotation_id=plateannotationlink.child
 $whereclause
 GROUP BY plate.id, annotation_mapvalue.index,
@@ -89,8 +89,8 @@ project.name as name, annotation_mapvalue.name as mapvalue_name,
 annotation_mapvalue.value as mapvalue_value,
 annotation_mapvalue.index as mapvalue_index
 from project
-inner join projectannotationlink on project.id =projectannotationlink.parent
-inner join annotation_mapvalue on
+left join projectannotationlink on project.id =projectannotationlink.parent
+left join annotation_mapvalue on
 annotation_mapvalue.annotation_id=projectannotationlink.child
 $whereclause
 GROUP BY project.id, annotation_mapvalue.index,
@@ -111,8 +111,8 @@ screen.name as name,screen.description,
 annotation_mapvalue.name as mapvalue_name,
 annotation_mapvalue.value as mapvalue_value,
 annotation_mapvalue.index as mapvalue_index from screen
-inner join screenannotationlink on screen.id =screenannotationlink.parent
-inner join annotation_mapvalue on
+left join screenannotationlink on screen.id =screenannotationlink.parent
+left join annotation_mapvalue on
 annotation_mapvalue.annotation_id=screenannotationlink.child
 $whereclause
 GROUP BY screen.id, annotation_mapvalue.index,
@@ -132,8 +132,8 @@ select well.id, well.owner_id, well.group_id,
 annotation_mapvalue.name as mapvalue_name,
 annotation_mapvalue.value as mapvalue_value,
 annotation_mapvalue.index as mapvalue_index from well
-inner join wellannotationlink on well.id =wellannotationlink.parent
-inner join annotation_mapvalue on
+left join wellannotationlink on well.id =wellannotationlink.parent
+left join annotation_mapvalue on
 annotation_mapvalue.annotation_id=wellannotationlink.child
 $whereclause
 GROUP BY well.id,  annotation_mapvalue.index,
