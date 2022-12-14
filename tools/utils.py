5cbc0101a9f2a61e4bb376effa8a5cc9e4dffc6f
khaledk2 <khaledk2@hotmail.com>
2022-12-14 15:39:39 +0000

    fix destination_folder variables

--- tools/utils.py
+++ tools/utils.py
@@ -28,9 +28,10 @@ def copy_tools_subfolder():
     subfolder = os.path.join(
         os.path.abspath(os.path.dirname(__file__)), "test_scripts"
     )
-    base_folder = "/etc/searchengine/"
-    if not os.path.isdir(base_folder):
+    destination_folder = "/etc/searchengine/"
+    if not os.path.isdir(destination_folder):
         destination_folder = os.path.expanduser("~")
+
     destination_folder=os.path.join(destination_folder, "test_scripts")
 
     if not os.path.isdir(destination_folder):
