4ff76ef870540ef9b1a5b5a30240bd5052e84e47
khaledk2 <khaledk2@hotmail.com>
2023-01-27 18:40:05 +0000

    Update test container key values

--- manage.py
+++ manage.py
@@ -386,10 +386,15 @@ def data_validator(screen_name=None, project_name=None):
     print(start2)
     print(end)
 
-def check_containers():
-    from omero_search_engine.validation.results_validator import container_keys_vakues
+#def check_containers():
+#    from omero_search_engine.validation.results_validator import container_keys_vakues
 
-    container_keys_vakues()
+def test_container_key_value():
+    from omero_search_engine.validation.results_validator import (
+        check_container_keys_vakues,
+    )
+
+    check_container_keys_vakues()
 
 
 if __name__ == "__main__":
