13dabf7f40b957b717721f5da629c36284465a81
khaledk2 <khaledk2@hotmail.com>
2023-09-02 21:53:05 +0100

    Support Rocky 9

---
+++ deployment/docker/rockylinux/run_app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "$@"
+
+#test if the configuration file exists, if not it will copy it from the app configuration folder
+test -f /etc/searchengine/.app_config.yml || cp /searchengine/configurations/app_config.yml /etc/searchengine/.app_config.yml
+
+#Check the script input
+if [[ $@ == run_app* ]] ; then
+  url_perfix=${@/run_app/}
+  echo using prefix: $url_perfix
+  bash start_gunicorn_serch_engine.sh $url_perfix
+elif [ -z  "$@" ] || [ "$@" = "run_app" ]; then
+  echo "Starting the app"
+  bash start_gunicorn_serch_engine.sh
+else
+  echo "$@"
+  python3.9 manage.py "$@"
+fi
---
+++ deployment/docker/rockylinux/start_gunicorn_serch_engine.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+NAME="omero_search_engine"
+USER root
+PYTHONPATH=~/.pyenv/versions/searchengine/bin/
+APPPATH=/searchengine
+SOCKFILE=/etc/searchengine/sock3 #change this to project_dir/sock (new file will be created)
+echo "Starting $NAME as `whoami`"
+export PATH="$PYTHONPATH:$PATH"
+export PATH="$APPPATH:$PATH"
+echo "staring the app"
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+echo "$RUNDIR"
+test -d $RUNDIR || mkdir -p $RUNDIR
+LOGS=/etc/searchengine/logs
+LOGSDIR=$(dirname $LOGS)
+test -d $LOGSDIR || mkdir -p $LOGSDIR
+user=$USER
+echo "Start Gunicorn ...."
+#exec ls -l  ~/.pyenv/versions/searchengine/bin/ 
+echo "$HOME"
+echo pwd
+cd $APPPATH
+if [ -z  "$@" ]; then
+  exec gunicorn "omero_search_engine:create_app('production')" -b 0.0.0.0:5577 --timeout 0 --name "$NAME"   --bind=unix:$SOCKFILE  --log-file=$LOGSDIR/logs/engine_gunilog.log --access-logfile=$LOGSDIR/logs/engine_access.log -error-logfile=$LOGSDIR/logs/engine_logs/engine_error.log  --workers 4
+else
+  echo Run with SCRIPT_NAME=$@
+  SCRIPT_NAME=/"$@"/ exec gunicorn "omero_search_engine:create_app('production')" -b 0.0.0.0:5577 --timeout 0 --name "$NAME"   --bind=unix:$SOCKFILE  --log-file=$LOGSDIR/logs/engine_gunilog.log --access-logfile=$LOGSDIR/logs/engine_access.log -error-logfile=$LOGSDIR/logs/engine_logs/engine_error.log  --workers 4
+fi
