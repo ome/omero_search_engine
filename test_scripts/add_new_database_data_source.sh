9b9353c71974061005d818e26555b00e116d48ff
khaledk2 <khaledk2@hotmail.com>
2025-05-21 23:40:14 +0100

    use var.txt to configure add_new_database_data_source.sh

--- test_scripts/add_new_database_data_source.sh
+++ test_scripts/add_new_database_data_source.sh
@@ -1,3 +1,3 @@
 source vars.txt
 echo  $SEARCHENGINE_IMAGE
-sudo docker run -v /data/searchengine/searchengine/:/etc/searchengine/  -v /data/searchengine/searchengine/logs/:/opt/app-root/src/logs/ --network searchengine-net $SEARCHENGINE_IMAGE set_database_configuration -u $data_base_url -s  5432 -n omeroreadonly -p $data_base_user_bassword -w omero_train -d omero_train -b omero_train.pgdump
+sudo docker run -v /data/searchengine/searchengine/:/etc/searchengine/  -v /data/searchengine/searchengine/logs/:/opt/app-root/src/logs/ --network searchengine-net $SEARCHENGINE_IMAGE set_database_configuration -u $data_base_url -s  $database_port_number -n $database_user_name -p  $data_base_user_bassword -w $working_data_source -d $database_name -b $backup_file_name
