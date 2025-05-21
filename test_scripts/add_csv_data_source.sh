cf233e8ee348069ebac282776cf5a3a618f2b5db
khaledk2 <khaledk2@hotmail.com>
2025-05-21 14:42:41 +0100

    Use new vars for new data source

--- test_scripts/add_csv_data_source.sh
+++ test_scripts/add_csv_data_source.sh
@@ -1,3 +1,4 @@
 source vars.txt
 echo  $SEARCHENGINE_IMAGE
-sudo docker run -v /data/searchengine/searchengine/:/etc/searchengine/  -v /data/searchengine/searchengine/logs/:/opt/app-root/src/logs/ --network searchengine-net $SEARCHENGINE_IMAGE set_data_source_files -n test_csv
+echo $NEW_DATA_SOURCE
+sudo docker run -v /data/searchengine/searchengine/:/etc/searchengine/  -v /data/searchengine/searchengine/logs/:/opt/app-root/src/logs/ --network searchengine-net $SEARCHENGINE_IMAGE set_data_source_files -n  $NEW_DATA_SOURCE
