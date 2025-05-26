ee326bede217a59748aaf2fbf7df2512e959e4e4
khaledk2 <khaledk2@hotmail.com>
2025-05-26 13:23:48 +0100

    Adress code review suggestion

--- test_scripts/delete_container.sh
+++ test_scripts/delete_container.sh
@@ -1,3 +1,3 @@
 source vars.txt
 echo  $SEARCHENGINE_IMAGE
-sudo docker run   -v /data/searchengine/searchengine/:/etc/searchengine/  -v /data/searchengine/searchengine/logs/:/opt/app-root/src/logs/ --network searchengine-net  $SEARCHENGINE_IMAGE delete_containers  -i $IDs -d $DATA_SOURCE -r $RESOURCE -u $UPDATE_CACHE -s True
+sudo docker run   -v /data/searchengine/searchengine/:/etc/searchengine/  -v /data/searchengine/searchengine/logs/:/opt/app-root/src/logs/ --network searchengine-net  $SEARCHENGINE_IMAGE delete_containers  -i $IDs -d $DATA_SOURCE -r $RESOURCE -u $UPDATE_CACHE -s $SYNCHRONOUS_RUN
