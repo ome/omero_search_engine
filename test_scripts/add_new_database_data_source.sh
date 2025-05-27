c3964dffe0a5fd0e50991682b51f42714b538797
khaledk2 <khaledk2@hotmail.com>
2025-05-27 20:32:10 +0100

    chnage variable to cpital letter

--- test_scripts/add_new_database_data_source.sh
+++ test_scripts/add_new_database_data_source.sh
@@ -1,3 +1,3 @@
 source vars.txt
 echo  $SEARCHENGINE_IMAGE
-sudo docker run -v /data/searchengine/searchengine/:/etc/searchengine/  -v /data/searchengine/searchengine/logs/:/opt/app-root/src/logs/ --network searchengine-net $SEARCHENGINE_IMAGE set_database_configuration -u $data_base_url -s  $database_port_number -n $database_user_name -p  $data_base_user_bassword -w $working_data_source -d $database_name -b $backup_file_name
+sudo docker run -v /data/searchengine/searchengine/:/etc/searchengine/  -v /data/searchengine/searchengine/logs/:/opt/app-root/src/logs/ --network searchengine-net $SEARCHENGINE_IMAGE set_database_configuration -u $dDATABASE_URL -s  $DATABASE_PORT_NUMBER -n $DATABASE_USERNAME -p  $DATABASE_USER_PASSWORD -w $WORKING_DATA_SOURCE -d $DATABASE_NAME -b $BACKUP_FILE_NAME
