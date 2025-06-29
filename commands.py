a31ab564cef83c742f2fc7d60b4f0b2bdbeed1e0
khaledk2 <khaledk2@hotmail.com>
2025-06-29 15:16:32 +0100

    Fix syntex error

--- commands.py
+++ commands.py
@@ -177,8 +177,9 @@ def get_index_data_from_database(resource, data_source, backup):
 
         # validate the indexing
 
-        test_indexing_search_query_("app_data/test_index_data.json", data_source_, False, True)
-
+        test_indexing_search_query_(
+            "app_data/test_index_data.json", data_source_, False, True
+        )
 
     # backup the index data
     if backup:
@@ -457,9 +458,15 @@ def test_indexing_search_query(
     if the data file, it will use sample file from
     (test_index_data.json) app_data folder
     """
-    test_indexing_search_query_( json_file,source,deep_check,check_studies,)
+    test_indexing_search_query_(
+        json_file,
+        source,
+        deep_check,
+        check_studies,
+    )
 
-def test_indexing_search_query_( json_file,source,deep_check,check_studies)
+
+def test_indexing_search_query_(json_file, source, deep_check, check_studies):
     from omero_search_engine.validation.results_validator import (
         validate_queries,
         test_no_images,
@@ -476,7 +483,6 @@ def test_indexing_search_query_( json_file,source,deep_check,check_studies)
         test_no_images(source)
     get_omero_stats()
     get_no_images_sql_containers(data_source=source)
-
 
 
 ##################################
