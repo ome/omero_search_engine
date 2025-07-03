c4585b8d773a0afbd459b87146b6e4ca98960266
khaledk2 <khaledk2@hotmail.com>
2025-07-03 21:56:48 +0100

    fix assert not equal

--- unit_tests/indexing_tests/test_delete_index_container.py
+++ unit_tests/indexing_tests/test_delete_index_container.py
@@ -72,7 +72,7 @@ class BasicTestCase(unittest.TestCase):
         # test delete container
         for id, container in containers_n.items():
             for con1 in containers_ad["results"]["results"]:
-                self.assertNotEquals(int(con1["id"]), int(id))
+                self.assertNotEqual(int(con1["id"]), int(id))
         from omero_search_engine.cache_functions.elasticsearch.transform_data import (
             index_container_from_database_,
         )
